3e88ededeafa622ceb597d700537976123b983ec
DJMendoza3 <DJM3webdev@gmail.com>
2023-03-22 09:23:53 -0500

    Added window resize event with context provider.

--- personal-portfolio/src/layout/container/Container.jsx
+++ personal-portfolio/src/layout/container/Container.jsx
@@ -1,3 +1,6 @@
+import { useContext, useEffect } from "react";
+import { WindowContext } from "providers/WindowProvider";
+
 import Navigation from "layout/navigation/Navigation";
 import Main from "sections/main/Main";
 import Skills from "sections/skills/Skills";
@@ -6,6 +9,20 @@ import About from "sections/about/About";
 import Contact from "sections/contact/Contact";
 
 export default function Container() {
+    const { setDimensions } = useContext(WindowContext);
+
+    useEffect(() => {
+        function handleResize() {
+            setDimensions({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, [setDimensions]);
+    
     return(
         <>
             <Navigation />
